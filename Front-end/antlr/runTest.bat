2ca7e26ec5f19d272a17733f4305434c46332512
cor14095 <ale.crts1996@gmail.com>
2017-04-04 03:01:01 -0500

    Testing stuff.

--- Front-end/antlr/runTest.bat
+++
@@ -1,2 +0,0 @@
-SET CLASSPATH=.;C:\Javalib\antlr-4.7-complete.jar;%CLASSPATH%
-python runSQL.py test.sql
--- Front-end/antlr/test.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM eshta WHERE (NOT(x='puto el que lo lea')); SELECT * FROM eshta WHERE (nepe+NOT);
